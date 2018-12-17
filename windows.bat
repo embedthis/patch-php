a5726c885bbb27fc1cc62d54d447800b97da020e
Michael O'Brien <mob@emobrien.com>
2018-12-17 19:52:50 +1100

    DEV: rename start to php.me

--- windows.bat
+++ windows.bat
@@ -7,16 +7,32 @@ SET PATH=wintools\bin;\perl64\bin;\perl\bin;%PATH%
 
 set VS=
 if DEFINED VSINSTALLDIR GOTO DONE
+
+for %%e in (Professional, Community) do (
+    for /l %%v in (2025, -1, 2017) do (
+        set VS=%%v
+        IF EXIST  "%PROGRAMFILES(x86)%\Microsoft Visual Studio\%%v\%%e\VC\Auxiliary\Build\vcvarsall.bat" call                "%PROGRAMFILES(x86)%\Microsoft Visual Studio\%%v\%%e\VC\Auxiliary\Build\vcvarsall.bat" %1
+        IF EXIST  "%PROGRAMFILES(x86)%\Microsoft Visual Studio\%%v\%%e\VC\Auxiliary\Build\vcvarsall.bat" goto :done
+        IF EXIST "%PROGRAMFILES%\Microsoft Visual Studio\%%v\%%e\VC\Auxiliary\Build\vcvarsall.bat" call                      "%PROGRAMFILES(x86)%\Microsoft Visual Studio\%%v\%%e\VC\Auxiliary\Build\vcvarsall.bat" %1
+        IF EXIST "%PROGRAMFILES%\Microsoft Visual Studio\%%v\%%e\VC\Auxiliary\Build\vcvarsall.bat" goto :done
+    )
+)
+
+
+set e=
 for /l %%v in (14, -1, 9) do (
     set VS=%%v
     IF EXIST "%PROGRAMFILES(x86)%\Microsoft Visual Studio %%v.0\VC\vcvarsall.bat" call "%PROGRAMFILES(x86)%\Microsoft Visual Studio %%v.0\VC\vcvarsall.bat" %1
     IF EXIST "%PROGRAMFILES(x86)%\Microsoft Visual Studio %%v.0\VC\vcvarsall.bat" goto :done
-    IF EXIST "%PROGRAMFILES%\Microsoft Visual Studio %%v.0\VC\vcvarsall.bat" call "%PROGRAMFILES%\Microsoft Visual Studio %%v.0\VC\vcvarsall.bat" %1
+    IF EXIST "%PROGRAMFILES%\Microsoft Visual Studio %%v.0\VC\vcvarsall.bat" call "%PROGRAMFILES%\Microsoft Visual Studio    %%v.0\VC\vcvarsall.bat" %1
     IF EXIST "%PROGRAMFILES%\Microsoft Visual Studio %%v.0\VC\vcvarsall.bat" goto :done
 )
-VS=
 :DONE
 
-echo Using Visual Studio %VS%.0
-echo %2 %3 %4 %5 %6 %7 %8 %9
+@echo.
+@echo Ran: "%PROGRAMFILES(x86)%\Microsoft Visual Studio\%%v\%%e\VC\Auxiliary\Build\vcvarsall.bat" %1
+@echo.
+@echo Using Visual Studio %VS% (v%VisualStudioVersion%) from %VSINSTALLDIR%
+@echo.
+@echo %2 %3 %4 %5 %6 %7 %8 %9
 %2 %3 %4 %5 %6 %7 %8 %9
